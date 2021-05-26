7ba15b58b8029c5f802bb7512bd88121cd9b57e6
gulyaev <gulyaev.gvs@gmail.com>
2021-05-26 22:11:28 +0200

    UseRef Learn useRef in file UseRef.js

---
+++ src/examples/UseRef.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect, useRef } from "react";
+
+export default function App() {
+  //const [renderCount, setRenderCount] = useState(1);
+  const [value, setValue] = useState("initial");
+  const renderCount = useRef(1);
+  const inputRef = useRef(null);
+  const prevValue = useRef("");
+
+  useEffect(() => {
+    renderCount.current++;
+
+    console.log(inputRef.current.value);
+  });
+
+  useEffect(() => {
+    prevValue.current = value;
+  }, [value]);
+
+  const focus = () => {
+    inputRef.current.focus();
+  };
+
+  return (
+    <div>
+      <h1>Количество рендеров: {renderCount.current}</h1>
+      <h2>Предыдущее состояние: {prevValue.current}</h2>
+      <input
+        ref={inputRef}
+        type="text"
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+      />
+      <button onClick={focus}>Фокус</button>
+    </div>
+  );
+}
